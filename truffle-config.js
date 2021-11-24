50f7fd1d90bee6ef540410608d90625fed122096
Larissa <lkp.schultze@gmail.com>
2021-11-24 19:47:10 +0100

    Adjustments to truffle-config

--- truffle-config.js
+++ truffle-config.js
@@ -6,7 +6,7 @@ module.exports = {
       network_id: "*" // Match any network id
     }
   },
-  // // To run contract with the latest compiler, uncomment lines 10-14 below:
+  // To run contract with the latest compiler, uncomment lines 10-14 below:
   compilers: {
     solc: {
       version: "^0.8",    // Fetch latest 0.8.x Solidity compiler 
